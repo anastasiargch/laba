395d8ec21ead2d95986c6083baf45b31b5000fc6
Anastasia Rogachevskaya <144005481+anastasiargch@users.noreply.github.com>
2023-10-16 10:03:24 +0300

    Update laba4B.cpp

--- laba4B.cpp
+++ laba4B.cpp
@@ -1,29 +1,27 @@
 #include <iostream>
 using namespace std;
 
-bool polindrom(string str) {
-    int len = str.length();
-    for (int i = 0; i < len/2; ++i) {
-        if (str[i] != str[len - i - 1])
-            return false;
-
+int my_strlen(char n) {
+    int count = 0;
+    while (n++) {
+       ++count;
     }
-    return true;
+    return count;
 }
+
 int main() {
-    const int max_length = 10;
-    char str[max_length];
-
-    cout << "Enter the string:" << endl;
-    cin.getline(str, max_length);
-    if(polindrom(str))
-    {
-        cout << "Word is polindrom.";
-    }
-    else
-    {
-        cout << "Word is not polindrom";
-    }
-
+    char str[300];
+    int len = my_strlen(*str);
+        cout << "Enter the string:" << endl;
+        cin.getline(str, 300);
+        for (int i = 0; i < len/2; i++) {
+            if (str[i] != str[len - i - 1]) {
+                cout << "Word is not palindrom" << endl;
+                break;
+            }
+            else {
+            cout << "Word is palindrom" << endl;
+            }
+        }   
     return 0;
 }
