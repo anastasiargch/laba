527197d04d5b629c801b057f1eeadf22aa934006
Anastasia Rogachevskaya <144005481+anastasiargch@users.noreply.github.com>
2023-10-16 16:37:27 +0300

    Update array.cpp

--- array.cpp
+++ array.cpp
@@ -13,8 +13,8 @@ int main() {
     cout << "Enter right border = ";
     cin >> right;
 
-   double arr[n];
-   int max_rand = 1000;
+    double* arr = new double [n];
+    int max_rand = 1000;
 
     cout << "Your array: ";
     for (int i = 0; i < n; i++) {
@@ -36,26 +36,29 @@ int main() {
             index = i;
             break;
         }
-        if (index == -1)
-            cout << "There are no negative elements in the array" << endl;
-        else {
-             int sum = 0;
+    if (index == -1)
+        cout << "There are no negative elements in the array" << endl;
+    else {
+        int sum = 0;
         for (int i = index + 1; i < n; i++)
             sum = sum + arr[i];
         cout << "Sum an integer part of the elements located after the last negative one: " << sum << endl;
     }
 
     cout << "Converted array:" << endl;
-    int _index=0;
-    for (int i = 0; i < n; i++) {
-        if ((arr[i] < P)) {
-            swap(arr[i], arr[_index]);
-            _index++;
+    double temp;
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - 1; j++) {
+            if (arr[j] >= P) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
         }
     }
 
     for (int i = 0; i < n; i++) {
         cout << arr[i] << "; ";
     }
-    return 0;
-}
+        return 0;
+ }
